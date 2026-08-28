c6a2dbd930b7a988c19ee0519f7a5cbb47b6c2fa
XandaoPe <73856144+XandaoPe@users.noreply.github.com>
2026-08-27 22:07:39 -0300

    correções

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -8,9 +8,12 @@ export const useSocket = (userId: string | null) => {
     useEffect(() => {
         if (!userId) return;
 
-        const newSocket = io('http://localhost:5000', {
+        const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+
+        const newSocket = io(API_URL, {
             transports: ['websocket'],
         });
+        
         socketRef.current = newSocket;
         setSocket(newSocket);
 
